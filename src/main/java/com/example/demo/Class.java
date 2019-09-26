99281aae18ac5f302b4501470c156073b3f5dc0e
mkelly1 <mkelly@his.com>
2019-09-26 14:33:45 -0400

    added responsive to table of classes

--- src/main/java/com/example/demo/Class.java
+++ src/main/java/com/example/demo/Class.java
@@ -1,5 +1,5 @@
 
-public class class{
+public class classes{
     private String id;
     private bolean available;
     private String department;
@@ -18,4 +18,60 @@ public class class{
         this.instructorName = instructorName;
         this.credits = credits;
         }
+
+    public String getId() {
+        return id;
     }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public bolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(bolean available) {
+        this.available = available;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    public void setInstructorName(String instructorName) {
+        this.instructorName = instructorName;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+}
