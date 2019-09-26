0cacf062ea2ddef4d05ad0d45d1c83b108d8b50a
mkelly1 <mkelly@his.com>
2019-09-26 11:06:18 -0400

    added class.html

---
+++ src/main/java/com/example/demo/Class.java
@@ -0,0 +1,21 @@
+
+public class class{
+    private String id;
+    private bolean available;
+    private String department;
+    private String title;
+    private String description;
+    private String instructorName;
+    private int credits;
+
+    public class(){}
+    public class(String id, boolean avail, String deptId, String title, String description, String instructorName, int credits ){
+        this.id = id;
+        this.available = avail;
+        this.department = deptId;
+        this.title = title;
+        this.description = description;
+        this.instructorName = instructorName;
+        this.credits = credits;
+        }
+    }
